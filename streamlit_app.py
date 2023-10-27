a40d3cc4a73803f56f5657ac6259584fcab50aa6
annagruszecka <144219056+annagruszecka@users.noreply.github.com>
2023-10-27 21:11:12 +0200

    Update streamlit_app.py
    change labels

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ def get_fruityvice_data(this_fruit_choice):
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
 
-streamlit.header("Fruityvice Fruit Advice!")
+streamlit.header("View Our Fruit List - Add Your Favorites")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
@@ -49,7 +49,7 @@ def get_fruit_load_list():
          return my_cur.fetchall()
 
 #Add the button to load the fruit 
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit List'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])     
    my_data_rows = get_fruit_load_list()
    streamlit.dataframe(my_data_rows)
